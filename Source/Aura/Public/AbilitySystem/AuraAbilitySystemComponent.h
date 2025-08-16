64caf67dd4ccbcf9cbee20268cc66c172773767c
chengreg <644076531@qq.com>
2025-08-16 23:59:40 +0800

    4. Ability System Component and Attribute Set

---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// ChenGangQiang All rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AuraAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
