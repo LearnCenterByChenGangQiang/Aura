7d8345ce1a3802aecdfee37eda2731c7a669cc35
chengreg <644076531@qq.com>
2025-08-17 18:01:34 +0800

    028 - 2. Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// ChenGangQiang All rights Reserved.
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// ChenGangQiang All rights Reserved.
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
