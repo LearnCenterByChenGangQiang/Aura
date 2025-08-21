2454bad593db777e0ea4250a626c41f60bbf0b75
Chen GangQiang <89298747+chengreg@users.noreply.github.com>
2025-08-21 18:32:12 +0800

    PreAttributeChange

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -4,6 +4,8 @@
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "Net/UnrealNetwork.h"
 
+#include "AuraDebugHelper.h"
+
 UAuraAttributeSet::UAuraAttributeSet()
 {
 	InitHealth(50.f);
@@ -20,6 +22,30 @@ void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimePropert
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp<float>(NewValue, 0.0f, GetMaxHealth());
+		//Debug::Print(FString::Printf(TEXT("Health:%f"), NewValue));
+	}
+	if (Attribute == GetMaxHealthAttribute())
+	{
+		//Debug::Print(FString::Printf(TEXT("MaxHealth:%f"), NewValue));
+	}
+	if (Attribute == GetManaAttribute())
+	{
+		NewValue = FMath::Clamp<float>(NewValue, 0.0f, GetMaxMana());
+		//Debug::Print(FString::Printf(TEXT("Mana:%f"), NewValue));
+	}
+	if (Attribute == GetMaxManaAttribute())
+	{
+		//Debug::Print(FString::Printf(TEXT("MaxMana:%f"), NewValue));
+	}
 }
 
 void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
