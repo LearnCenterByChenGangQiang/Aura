0cdc76a66834d3dde3a6a8acec2c88cdc32360c0
Chen GangQiang <89298747+chengreg@users.noreply.github.com>
2025-08-25 14:37:20 +0800

    079 Player Level and Combat Interface

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -17,9 +17,12 @@ class AURA_API AAuraCharacter : public AAuraCharacterBase
 public:
 	AAuraCharacter();
 
-public:
 	virtual void PossessedBy(AController* NewController) override;
 	virtual void OnRep_PlayerState() override;
+
+	//~ Begin Combat Interface
+	virtual int32 GetPlayerLevel() override;
+	//~ End Combat Interface
 
 private:
 	virtual void InitAbilityActorInfo() override;
