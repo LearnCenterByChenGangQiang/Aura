34b65f1b6189c56d8d60d646b2ac5f14aeb82ff7
Chen GangQiang <89298747+chengreg@users.noreply.github.com>
2025-08-26 18:32:20 +0800

    097 Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// ChenGangQiang All rights Reserved.
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+
+}
