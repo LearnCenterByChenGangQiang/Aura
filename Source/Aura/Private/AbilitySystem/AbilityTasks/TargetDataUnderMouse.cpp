e6ec324180e9805e432188e06a675df8f1149f9e
Chen GangQiang <89298747+chengreg@users.noreply.github.com>
2025-08-28 17:23:40 +0800

    127 Receiving Target Data

--- Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -21,7 +21,14 @@ void UTargetDataUnderMouse::Activate()
 	}
 	else
 	{
-		
+		const FGameplayAbilitySpecHandle SpecHandle = GetAbilitySpecHandle();
+		const FPredictionKey ActivationPredictionKey = GetActivationPredictionKey();
+		AbilitySystemComponent.Get()->AbilityTargetDataSetDelegate(SpecHandle, ActivationPredictionKey).AddUObject(this, &UTargetDataUnderMouse::OnTargetDataReplicatedCallback);
+		const bool bCalledDelegate = AbilitySystemComponent.Get()->CallReplicatedTargetDataDelegatesIfSet(SpecHandle, ActivationPredictionKey);
+		if (bCalledDelegate)
+		{
+			SetWaitingOnRemotePlayerData();
+		}
 	}
 	
 	
@@ -51,4 +58,16 @@ void UTargetDataUnderMouse::SendMouseCursorData()
 	{
 		ValidData.Broadcast(DataHandle);
 	}
+}
+
+void UTargetDataUnderMouse::OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle,
+	FGameplayTag ActivationTag)
+{
+	AbilitySystemComponent->ConsumeClientReplicatedTargetData(GetAbilitySpecHandle(), GetActivationPredictionKey());
+
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
+	
 }
--- Source/Aura/Private/AuraAssetManager.cpp
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -3,6 +3,8 @@
 
 #include "AuraAssetManager.h"
 #include "AuraGamplayTags.h"
+#include "AbilitySystemGlobals.h"
+
 
 UAuraAssetManager& UAuraAssetManager::Get()
 {
@@ -17,4 +19,7 @@ void UAuraAssetManager::StartInitialLoading()
 	Super::StartInitialLoading();
 
 	FAuraGameplayTags::InitializeNativeGameplayTags();
+
+	// This is required to use target data!
+	UAbilitySystemGlobals::Get().InitGlobalData();
 }
